71e52458b21bef757dde209f32acb035d4f8d2e0
Guillermo Villalta <mexcelus@gmail.com>
2019-07-15 09:52:40 -0600

    added update guessed letters on html and update remaining number of guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,28 @@ var marioHangman = {
     remainGuess: 12,
     userTextWins: document.getElementById("numberOfWins"),
     userTextMystery: document.getElementById("mysteryWord"),
+    userTextLetters: document.getElementById("guessedLetters"),
     generateWord: function (array) {
         var guess = array[Math.floor(Math.random() * array.length)];
         this.userTextMystery.textContent = guess;
         return guess;
+    },
+    updateGuessedLetters: function (guess) {
+        var node = document.createTextNode(guess + ' ')
+        this.userTextLetters.appendChild(node);
+    },
+    updateRemainGuess: function () {
+        this.remainGuess--;
+        console.log(this.remainGuess);
     }
 }
 
-console.log(marioHangman.generateWord(marioHangman.mysteryArray));
+window.onload = function () {
+    console.log(marioHangman.generateWord(marioHangman.mysteryArray));
+}
+
+document.onkeyup = function (event) {
+    var guess = event.key;
+    marioHangman.updateGuessedLetters(guess);
+    marioHangman.updateRemainGuess();
+}
