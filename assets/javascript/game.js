726989959f5823bcd69640ba68a88d00aa2688fa
Guillermo Villalta <mexcelus@gmail.com>
2019-07-15 08:28:30 -0600

    show random word on html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,11 @@ var marioHangman = {
     mysteryArray: ['babymario', 'birdo', 'boo', 'bowser', 'goomba', 'koopatroopa', 'luigi', 'mario', 'peach', 'shyguy', 'starman', 'toad', 'waluigi', 'wario', 'yoshi'],
     numberOfWins: 0,
     remainGuess: 12,
+    userTextWins: document.getElementById("numberOfWins"),
+    userTextMystery: document.getElementById("mysteryWord"),
     generateWord: function (array) {
         var guess = array[Math.floor(Math.random() * array.length)];
+        this.userTextMystery.textContent = guess;
         return guess;
     }
 }
